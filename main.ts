5f8fd0befab0f5a846d05bca358669ae667f750a
Kaito Udagawa <umireon@gmail.com>
2022-09-15 12:18:49 +0900

    Build

--- main.ts
+++ main.ts
@@ -1,25 +1,24 @@
-import * as core from '@actions/core';
+import * as core from "@actions/core";
 import { promisify } from "node:util";
 import { exec } from "node:child_process";
-import { access, rm } from "node:fs/promises"
+import { access, rm } from "node:fs/promises";
 
-const execPromise = promisify(exec)
+const execPromise = promisify(exec);
 
-const CACHE_DIR = '/Users/admin/caches';
+const CACHE_DIR = "/Users/admin/caches";
 
-async function main () {
-    const path = core.getInput('path', { required: true });
-    const key = core.getInput('key', { required: true });
+async function main() {
+  const path = core.getInput("path", { required: true });
+  const key = core.getInput("key", { required: true });
 
-    try {
-        await access(`${CACHE_DIR}/${key}.tar.xz`);
-    } catch (err) {
-        return;
-    }
+  try {
+    await access(`${CACHE_DIR}/${key}.tar.xz`);
+  } catch (err) {
+    return;
+  }
 
-    await rm(path, { force: true, recursive: true })
-    await execPromise(`tar -cJf ${CACHE_DIR}/${key}.tar.xz ${path}`);
+  await rm(path, { force: true, recursive: true });
+  await execPromise(`tar -cJf ${CACHE_DIR}/${key}.tar.xz ${path}`);
 }
 
-main()
-
+main();
